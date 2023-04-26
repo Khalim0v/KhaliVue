db5d195310b154849377746a890c2660a827d623
Khalim0v <mrkhalimov10@gmail.com>
2023-04-26 12:52:24 +0500

    modified Login

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -4,22 +4,61 @@
       <img :src="logo" alt="logo" style="width: 100px; cursor: pointer;" @click="toHomeHandler" />
       <h1 class="h3 mb-3 fw-normal mt-2">Login</h1>
 
-      <Input :label="'Email address'" :type="'email'" />
-      <Input :label="'Password'" :type="'password'" />
+      <ValidationError v-if="validationErrors" :validationErrors="validationErrors" />
 
-      <Button type="submit">Log in</Button>
+      <Input :label="'Email address'" :type="'email'" v-model="email" />
+      <Input :label="'Password'" :type="'password'" v-model="password" />
+
+      <Button type="submit" :disabled="isLoading" @click="submitHandler">Login</Button>
     </form>
   </main>
 </template>
 
 <script>
+import { mapState } from 'vuex'
 import { logo } from '../constants'
+import ValidationError from './ValidationError.vue'
 export default {
   data() {
     return {
-      logo
+      logo,
+      username: '',
+      email: '',
+      password: '',
+
     }
   },
+  components: {
+    ValidationError
+  },
+  computed: {
+    ...mapState({  //vuex bilan ishlaganda computed ishlatilishi`
+      isLoading: state => state.auth.isLoading,
+      validationErrors: state => state.auth.errors
+    })
+    //isLoading() {
+    //  return this.$store.state.auth.isLoading
+    //},
+    //validationErrors() {
+    //  return this.$store.state.auth.errors
+    //}
+  },
+  methods: {
+    submitHandler(e) {
+      e.preventDefault() //page refresh bolib ketmasligi uchun event ga preventDefault qiymat beriladi
+      const data = {
+        //username: this.username,
+        email: this.email,
+        password: this.password,
+      }
+      this.$store.dispatch('login', data)
+        .then(user => {
+          console.log("USER", user)
+          this.$router.push({ name: 'home' })
+        })
+        .catch(err => console.log("ERROR", err)) //payload - dispatch qilyotgandagi ikkinchi parametr nomi
+    }
+  }
 }
 </script>
 
--- src/modules/auth.js
+++ src/modules/auth.js
@@ -21,10 +21,23 @@ const mutations = { //mutatsalar bilan ishlayotganda mutatsalarni commit qilinad
     state.isLoading = false
     state.errors = payload.errors
   },
+  loginStart(state) {
+    state.isLoading = true
+    state.user = null
+    state.errors = null
+  },
+  loginSuccess(state, payload) {
+    state.isLoading = false
+    state.user = payload
+  },
+  loginFailure(state, payload) {
+    state.isLoading = false
+    state.user = payload
+  },
 }
 
 const actions = { //action lar bilan ishlayotganda action lar dispatch qilinadi
-  register(context, user) {
+  register(context, user) { //context commit qilish uchun user esa parametrga data larni berish uchun
     return new Promise((resolve, reject) => {
       context.commit('registerStart')
       AuthService.register(user)
@@ -35,6 +48,21 @@ const actions = { //action lar bilan ishlayotganda action lar dispatch qilinadi
         })
         .catch(error => {
           context.commit('registerFailure', error.response.data)
+          reject(error.response.data)
+        })
+    })
+  },
+  login(context, user) { // context bizga comment qilishimiz uchun kerak; user esa payload parametrga data larni berish uchun kerak
+    return new Promise((resolve, reject) => {
+      context.commit('loginStart')
+      AuthService.login(user)
+        .then(response => {
+          context.commit('loginSuccess', response.data.user)
+          setItem('token', response.data.user.token)
+          resolve(response.data.user)
+        })
+        .catch(error => {
+          context.commit('loginFailure', error.response.data)
           reject(error.response.data)
         })
     })
