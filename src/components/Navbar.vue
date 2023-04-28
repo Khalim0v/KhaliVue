f444f4905d7a1acad163963b67fb3a2d12c8b99e
Khalim0v <mrkhalimov10@gmail.com>
2023-04-28 17:52:36 +0500

    Navigation navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -5,18 +5,32 @@
     </a>
 
     <nav class="d-inline-flex mt-2 mt-md-0 ms-md-auto">
-      <RouterLink :to="{ name: 'login' }" class="me-3 py-2 text-dark text-decoration-none">Login</RouterLink>
-      <RouterLink :to="{ name: 'register' }" class="me-3 py-2 text-dark text-decoration-none">Register</RouterLink>
+      <template v-if="isLoggedIn">
+        <!--bu template kod yozvotkanda har xil shartli operator(condition)lar qoyishimiz uchun qulay-->
+        <RouterLink :to="{ name: 'login' }" class="me-3 py-2 text-dark text-decoration-none">{{ user.username }}
+        </RouterLink>
+      </template>
+      <template v-if="!isLoggedIn">
+        <RouterLink :to="{ name: 'login' }" class="me-3 py-2 text-dark text-decoration-none">Login</RouterLink>
+        <RouterLink :to="{ name: 'register' }" class="me-3 py-2 text-dark text-decoration-none">Register</RouterLink>
+      </template>
     </nav>
   </div>
 </template>
 <script>
+import { mapState } from 'vuex'
 import { logo } from '../constants'
 export default {
   data() {
     return {
       logo
     }
+  },
+  computed: {
+    ...mapState({
+      user: state => state.auth.user,
+      isLoggedIn: state => state.auth.isLoggedIn,
+    })
   },
   methods: {
     toHomeHandler() {
