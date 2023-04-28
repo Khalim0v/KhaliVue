2c8bcde37b685dfd73bdbda7c5baae898eb8f4e4
Khalim0v <mrkhalimov10@gmail.com>
2023-04-28 17:54:51 +0500

    Navigation navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -7,7 +7,7 @@
     <nav class="d-inline-flex mt-2 mt-md-0 ms-md-auto">
       <template v-if="isLoggedIn">
         <!--bu template kod yozvotkanda har xil shartli operator(condition)lar qoyishimiz uchun qulay-->
-        <RouterLink :to="{ name: 'login' }" class="me-3 py-2 text-dark text-decoration-none">{{ user.username }}
+        <RouterLink :to="{ name: 'login' }" class="me-3 py-2 text-dark text-decoration-none">{{ currentUser.username }}
         </RouterLink>
       </template>
       <template v-if="!isLoggedIn">
@@ -28,7 +28,7 @@ export default {
   },
   computed: {
     ...mapState({
-      user: state => state.auth.user,
+      currentUser: state => state.auth.user,
       isLoggedIn: state => state.auth.isLoggedIn,
     })
   },
